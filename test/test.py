a37bdd590a324e6b256e569018d5b760b31b64cf
Alessandro Usseglio Viretta <aus@auv.name>
2013-03-15 15:35:58 +0100

    change 6 (git)

--- test/test.py
+++ test/test.py
@@ -8,10 +8,12 @@ from calc import *
 def test_plus():
     assert plus(1, 2) == 3
 
+
 def test_minus():
     assert minus(3, 2) == 1
+
 
 def test_times():
     assert times(2, 3) == 6
 
-#  change 1
+#  change 6
